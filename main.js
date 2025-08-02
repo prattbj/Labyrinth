4805b256c99e860e47b188e4dfe70ce053ab0f8d
prattbj <prattbj@users.noreply.github.com>
2025-08-02 23:15:11 +0000

    deploy: 59c32ef28e686799c5936a9f701acf1f94ca36e9

--- main.js
+++ main.js
@@ -1,26 +1,4 @@
-﻿// import { dotnet } from './_framework/dotnet.js'
-
-// const { getAssemblyExports, getConfig } = await dotnet
-//     .withDiagnosticTracing(false)
-//     .create();
-
-// const config = getConfig();
-// const exports = await getAssemblyExports(config.mainAssemblyName);
-
-// dotnet.instance.Module['canvas'] = document.getElementById('canvas');
-
-// function mainLoop() {
-//     exports.Labyrinth.Program.RunGame();
-
-//     window.requestAnimationFrame(mainLoop);
-// }
-
-// await dotnet.run();
-// window.requestAnimationFrame(mainLoop);
-
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-import { dotnet } from './_framework/dotnet.js'
+﻿import { dotnet } from './_framework/dotnet.js'
 const canvas = document.getElementById('canvas');
 canvas.style.position = "absolute";
 canvas.style.top = 0;
